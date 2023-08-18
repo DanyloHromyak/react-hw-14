bd3af6b08a90ad4efa94343fc6779b7a9c41811b
Danylo <danylohromyak12@gmail.com>
2023-08-19 00:02:18 +0300

    animations added

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,4 +1,5 @@
 import { Typography, Button } from "@mui/material";
+import { motion, AnimatePresence } from "framer-motion";
 
 function ContactList({ contacts, filter, deleteContact }) {
   const filteredContacts = contacts.filter(contact =>
@@ -9,18 +10,36 @@ function ContactList({ contacts, filter, deleteContact }) {
     <Typography marginTop="20px" variant="h3" contacts={contacts}>
       Contacts
       <ul>
-        {filteredContacts.map(contact => (
-          <li key={contact.id}>
-            {contact.name}: {contact.number}
-            <Button
-              onClick={() => deleteContact(contact.id)}
-              style={{ margin: "20px" }}
-              variant="contained"
-              size="small">
-              Remove
-            </Button>
-          </li>
-        ))}
+        <AnimatePresence>
+          {filteredContacts.map(contact => (
+            <motion.li
+              key={contact.id}
+              initial={{
+                opacity: 0,
+                translateX: "100%",
+              }}
+              animate={{
+                opacity: 1,
+                translateX: 0,
+              }}
+              exit={{
+                opacity: 0,
+                translateX: "100%",
+              }}
+              transition={{
+                duration: 0.5,
+              }}>
+              {contact.name}: {contact.number}
+              <Button
+                onClick={() => deleteContact(contact.id)}
+                style={{ margin: "20px" }}
+                variant="contained"
+                size="small">
+                Remove
+              </Button>
+            </motion.li>
+          ))}
+        </AnimatePresence>
       </ul>
     </Typography>
   );
